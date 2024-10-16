fdb1053d972733944aa3c090252d147dc4ad93d8
Armel K. Dovenon <dove.armel@gmail.com>
2024-10-16 12:10:14 +0100

    création du bouton d'ajout de hashtag et mise en place du mode d'ajout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,17 +18,55 @@ late final PageController _pageController;
 @override
   void initState() {
     super.initState();
-
     _pageController = PageController(initialPage: _currentIndex);
   }
 
-
   @override
   void dispose() {
-    
     _pageController.dispose();
     super.dispose();
   }
+
+  void _modal(BuildContext context) => showModalBottomSheet(
+      context: context, 
+      builder: (context) =>  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          children:  <Widget>[
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Text("Hashtag creator", style: Theme.of(context).textTheme.titleLarge ),
+             ),
+             const Padding(
+               padding: EdgeInsets.all(8.0),
+               child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Entrer votre hashtag",
+                ),
+                         ),
+             ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("Annuler".toUpperCase()),
+                
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("Ajouter".toUpperCase()),
+                
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   
 
   @override
@@ -47,6 +85,12 @@ late final PageController _pageController;
           ListHashtagView(),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _modal(context),
+        child: const Icon(
+          Icons.add
+        ),
+       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
