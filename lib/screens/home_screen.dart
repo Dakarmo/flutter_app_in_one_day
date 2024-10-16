cd1eb6c44dd51e21c80db3b71519acb85e099873
Armel K. Dovenon <dove.armel@gmail.com>
2024-10-16 10:13:38 +0100

    installation du projet et mise en place de l'interface utilisateur

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_in_one_day/screens/views/home_view.dart';
+import 'package:flutter_app_in_one_day/screens/views/list_hashtag_view.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+ final PageController _pageController = PageController(initialPage: 0);
+  
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    
+    _pageController.dispose();
+    super.dispose();
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: PageView(
+        onPageChanged: (index) {
+            setState( () => _currentIndex = index);
+          },
+        controller: _pageController,
+        children: const <Widget> [
+          HomeView(),
+          ListHashtagView(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+
+          setState(() => _currentIndex = index);
+
+          _pageController.jumpToPage(index);
+        },
+          
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Hashtags',
+          ),
+        ]
+      ),
+    );
+  }
+}
